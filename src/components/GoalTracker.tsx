39b39382ede050ce6dc4246abd22a7d5294d0296
Madekuji <27665110+Madekuji@users.noreply.github.com>
2025-04-03 12:20:01 +0800

    table fix on mobile

--- src/components/GoalTracker.tsx
+++ src/components/GoalTracker.tsx
@@ -10,28 +10,28 @@ function GoalTracker() {
             <div className="goals">
                 <div id="goalsTable">
                     <div className="goalsTableRow">
-                        <span><b>Minimum Goal (due April 14, 2025):</b> 20-second ad on April 26, 2025</span>
-                        <span>PHP 78,400 ~ USD 1,500</span>
+                        <span className="tableLeft"><b>Minimum Goal (due April 14, 2025):</b> 20-second ad on April 26, 2025</span>
+                        <span className="tableRight">PHP 78,400 ~USD 1,500</span>
                     </div>
                     <div className="goalsTableRow">
-                        <span><b>Golden Goal:</b> 20-second ad on April 26, 2025</span>
-                        <span>PHP 117,600 ~ USD 2,200</span>
+                        <span className="tableLeft"><b>Golden Goal:</b> 20-second ad on April 26, 2025</span>
+                        <span className="tableRight">PHP 117,600 ~USD 2,200</span>
                     </div>
                     <div className="goalsTableRow">
                         <span><b>Bonus Goals:</b></span>
                         <span></span>
                     </div>
                     <div className="goalsTableRow">
-                        <span>30-second ad on April 26 & 27, 2025</span>
-                        <span>PHP 235,200 ~ USD 4,300</span>
+                        <span className="tableLeft">30-second ad on April 26 & 27, 2025</span>
+                        <span className="tableRight">PHP 235,200 ~USD 4,300</span>
                     </div>
                     <div className="goalsTableRow">
-                        <span>30-second ad on April 26, 27 & May 2, 2025</span>
-                        <span>Php 352,800 ~ USD 6,500</span>
+                        <span className="tableLeft">30-second ad on April 26, 27 & May 2, 2025</span>
+                        <span className="tableRight">PHP 352,800 ~ USD 6,500</span>
                     </div>
                     <div className="goalsTableRow">
-                        <span><b>WE MOOM GOAL:</b> 30-second ads for an entire week</span>
-                        <span>PHP 604,800 ~ USD 11,000</span>
+                        <span className="tableLeft"><b>WE MOOM GOAL:</b> 30-second ads for an entire week</span>
+                        <span className="tableRight">PHP 604,800 ~ USD 11,000</span>
                     </div>
                     <div className="goalsTableRow">
                         <br />*note: airdate is subject to change depending on airslot availability.
