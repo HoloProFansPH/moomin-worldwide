eb2824016ea8217f864d0e76bde43cf29d5608a5
Madekuji <27665110+Madekuji@users.noreply.github.com>
2025-04-04 12:49:40 +0800

    fixed ios safari bg again

--- src/components/HomeDescription.tsx
+++ src/components/HomeDescription.tsx
@@ -2,9 +2,7 @@ function HomeDescription() {
     return(
         <div className="card mainCard">
             <p>
-                <b>Moomin' Worldwide</b> is a crowdfunding campaign to put Nanashi Mumei on the MOA GLOBE, featuring art
-                created by
-                the Global Hooman Community.<br/>
+                <b>Moomin' Worldwide</b> is a crowdfunding campaign that aims to put Nanashi Mumei on a spherical digital billboard in the Philippines featuring art by the Global Hooman Community.<br/>
                 <br/>
                 <b>Target Date: April 26, 2025 (PHT)</b><br/>
                 Additional dates will be added depending on the campaign's success.<br/>
